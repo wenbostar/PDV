94a4fb9740834fa509cf33c6f5776383de9ae212
KaiLiCn <likaicnsh@gmail.com>
2023-11-25 17:07:37 -0500

    Update 1.8.2

--- src/main/java/PDVGUI/gui/PDVMainClass.java
+++ src/main/java/PDVGUI/gui/PDVMainClass.java
@@ -262,7 +262,7 @@ public class PDVMainClass extends JFrame {
     /**
      * Version
      */
-    private static final String VERSION = "1.8.1";
+    private static final String VERSION = "1.8.2";
 
     /**
      * Main class
